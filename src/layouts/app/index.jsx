9fe7447ad025fd727afcd283373994b332eb3d9e
dngwoo <akfmzk84@naver.com>
2020-11-20 16:16:06 +0900

    react-router-dom 설치 및 페이지 생성

--- src/layouts/app/index.jsx
+++ src/layouts/app/index.jsx
@@ -1,16 +1,21 @@
 import "./style.css";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "../../pages/home";
+import VideoDetail from "../../pages/video-detail";
+import Header from "../header";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" />
-        <Route path="/search" />
-        <Route path="/:id" />
-      </Switch>
-    </Router>
+    <>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/video/:id" component={VideoDetail} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/video-detail/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const VideoDetail = () => <div>video-detail page</div>;
+
+export default VideoDetail;
